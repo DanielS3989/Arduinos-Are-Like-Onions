5cefe2c221362c19278c1dadfd9b0e9701383161
hoozkrispe <ds.game978@gmail.com>
2022-04-07 14:53:44 -0500

    Update proj2_04062022.ino
    Created variables and initialized servo and button pins

--- proj2_04062022/proj2_04062022.ino
+++ proj2_04062022/proj2_04062022.ino
@@ -1,5 +1,21 @@
+/*
+   AET Shrek Enclosure
+   Team Name: Arduinos Are Like Onions
+   Team Members: Jack Alsaffar, Joshua Hale, Daniel Sanchez
+   Description: Switch and servo program
+*/
+
+#include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttonPin = 7;
+
+
 void setup() {
   // put your setup code here, to run once:
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 
 }
 
